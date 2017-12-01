56d548fa001339cde0e4568fb47d9e1f276f6c6f
Katuwal-Deepak <you@example.com>
2017-12-01 17:02:45 +0530

    react-native-swiper added

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,12 +4,39 @@ import {
     FlatList, StyleSheet, Dimensions, ScrollView,
     TouchableOpacity, Button, ToolbarAndroid, Modal
 } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 export default class Home extends Component {
+    renderSwiper = () => {
+        const { sliderContainerStyle, containerStyle } = styles;
+
+        return (
+            <View style={sliderContainerStyle}>
+                <Swiper style={styles.wrapper} autoplay={true}>
+                    <Image
+                        source={{ uri: 'http://carzonemumbai.com/images/ddfb1335c2d0966fb79e964d02948d7e20130816_141807.jpg' }}
+                        style={styles.slide1}
+                    />
+                    <Image
+                        source={{ uri: 'http://st.motortrend.com/uploads/sites/10/2015/11/2016-hyundai-elantra-sport-sedan-angular-front2.png' }}
+                        style={styles.slide1}
+                    />
+                    <Image
+                        source={{ uri: 'http://gomotorplace.com/wp-content/uploads/2016/12/Ford-Fiestav.jpg' }}
+                        style={styles.slide1}
+                    />
+                </Swiper>
+            </View>
+        );
+    }
 
     render() {
+        const { sliderContainerStyle, containerStyle } = styles;
+
         return (
-            <View><Text>HELLO</Text></View>
+            <View style={containerStyle}>
+                {this.renderSwiper()}
+            </View>
         );
     }
 
@@ -20,8 +47,19 @@ const categoryContainerHeight = (160 / 768) * window.height;
 
 
 const styles = StyleSheet.create({
-    container: {
+    containerStyle: {
         flex: 1,
         flexDirection: 'column'
-    }
+    },
+    sliderContainerStyle: {
+        height: (350 / 768) * window.height
+    },
+    wrapper: {
+    },
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB',
+    },
 });
