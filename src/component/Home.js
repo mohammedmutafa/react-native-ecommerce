a840d7ba83c452ab1b6267cef2d3c967e598573f
Katuwal-Deepak <you@example.com>
2017-12-02 10:10:35 +0530

    Dynamic Swiper Images

--- src/component/Home.js
+++ src/component/Home.js
@@ -14,15 +14,20 @@ export default class Home extends Component {
             <View style={sliderContainerStyle}>
                 <Swiper style={styles.wrapper} autoplay={true}>
                     <Image
-                        source={{ uri: 'http://carzonemumbai.com/images/ddfb1335c2d0966fb79e964d02948d7e20130816_141807.jpg' }}
+                        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/innernepal-dca5b.appspot.com/o/travelBanner.png?alt=media&token=9cb6ab5e-229e-4308-b7a0-5835936e1635' }}
                         style={styles.slide1}
                     />
                     <Image
-                        source={{ uri: 'http://st.motortrend.com/uploads/sites/10/2015/11/2016-hyundai-elantra-sport-sedan-angular-front2.png' }}
+                        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/innernepal-dca5b.appspot.com/o/fashion.jpg?alt=media&token=3426181c-22fa-43f1-aac3-177b20676bb5' }}
                         style={styles.slide1}
                     />
                     <Image
-                        source={{ uri: 'http://gomotorplace.com/wp-content/uploads/2016/12/Ford-Fiestav.jpg' }}
+                        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/innernepal-dca5b.appspot.com/o/bikesBanner.png?alt=media&token=1bc56821-8262-4bfe-b67d-2a6129ec87d3' }}
+                        style={styles.slide1}
+                    />
+                    <Image
+                        //Flag URL: https://firebasestorage.googleapis.com/v0/b/innernepal-dca5b.appspot.com/o/nepal.png?alt=media&token=aa5bcfc5-8a6a-4b69-92c8-2d208b2fc2b9
+                        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/innernepal-dca5b.appspot.com/o/basketBanner.png?alt=media&token=adeb788e-66df-448f-959d-db20b07f423c' }}
                         style={styles.slide1}
                     />
                 </Swiper>
@@ -73,6 +78,6 @@ const styles = StyleSheet.create({
         position: 'absolute', // position it in circle
         bottom: 0, // position it in circle
         marginLeft: window.width / 2, // center it in main view same value as marginLeft for circle but positive
-        backgroundColor: '#9DD6EB',
+        backgroundColor: '#FFFFFF',
     }
 });
