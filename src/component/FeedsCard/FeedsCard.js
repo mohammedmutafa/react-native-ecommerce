477740b178fc21e19161e0c84fdec0c9068ddad2
katwal-dipak <katwal.dipak@hotmail.com>
2018-03-17 13:49:41 +0530

    minor changes

--- src/component/FeedsCard/FeedsCard.js
+++ src/component/FeedsCard/FeedsCard.js
@@ -8,7 +8,7 @@ import {
     TouchableOpacity
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { Avatar } from 'react-native-elements';
+import { Avatar, Icon } from 'react-native-elements';
 
 import styles from './styles';
 import colors from '../../styles/Color';
@@ -17,54 +17,82 @@ const {
     container,
     avatarContainer,
     profileConatiner,
+    imageViewContainerStyle,
     imageViewStyle,
     nameTextStyle,
     productTitleTextStyle,
     detailContainerStyle
 } = styles;
 
-export const FeedsCard = ({ name, title, time, imageURL, thumbnailURL }) => (
+export class FeedsCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookmarked: false
+        }
+    }
 
-    <View style={container}>
-        <View style={detailContainerStyle}>
-            <View style={avatarContainer}>
-                <View style={profileConatiner}>
-                    <Avatar
-                        small
-                        rounded
-                        source={{ uri: thumbnailURL }}
-                        // onPress={() => console.log("Works!")}
-                        activeOpacity={0.7}
+    onPressBookmarkButton = () => {
+        this.setState({ bookmarked: !this.state.bookmarked });
+    }
+
+    render() {
+        const { name, title, time, imageURL, thumbnailURL } = this.props;
+        const { bookmarked } = this.state;
+
+        return (
+            <View style={container}>
+                <View style={detailContainerStyle}>
+                    <View style={avatarContainer}>
+                        <View style={profileConatiner}>
+                            <Avatar
+                                small
+                                rounded
+                                source={{ uri: thumbnailURL }}
+                                // onPress={() => console.log("Works!")}
+                                activeOpacity={0.7}
+                            />
+                            <Text
+                                style={nameTextStyle}
+                                ellipsizeMode='tail'
+                                numberOfLines={1}
+                            >
+                                {name}
+                            </Text>
+                        </View>
+                        <Text
+                            style={{ alignSelf: 'flex-end', color: colors.lightDark, fontSize: 10 }}
+                            ellipsizeMode='tail'
+                            numberOfLines={1}
+                        >{time}
+                        </Text>
+                    </View>
+                </View>
+                <View style={imageViewContainerStyle}>
+                    <Image
+                        source={{ uri: imageURL }}
+                        style={imageViewStyle}
                     />
-                    <Text
-                        style={nameTextStyle}
-                        ellipsizeMode='tail'
-                        numberOfLines={1}
-                    >
-                        {name}
-                    </Text>
+                    <Icon
+                        name={bookmarked ? 'heart' : 'heart-outlined'}
+                        type="entypo"
+                        color={bookmarked ? colors.lightWhite : colors.lightDark}
+                        containerStyle={{ position: 'absolute', alignSelf: 'flex-end', padding: 10 }}
+                        onPress={this.onPressBookmarkButton}
+                    />
                 </View>
                 <Text
-                    style={{ alignSelf: 'flex-end', color: colors.lightDark, fontSize: 10 }}
+                    style={productTitleTextStyle}
                     ellipsizeMode='tail'
-                    numberOfLines={1}
-                >{time}
+                    numberOfLines={2}
+                >
+                    {title}
                 </Text>
             </View>
-        </View>
-        <Image
-            source={{ uri: imageURL }}
-            style={imageViewStyle}
-        />
-        <Text
-            style={productTitleTextStyle}
-            ellipsizeMode='tail'
-            numberOfLines={2}
-        >
-            {title}
-        </Text>
-    </View>
-);
+
+        );
+    }
+}
 
 FeedsCard.propTypes = {
     time: PropTypes.string,
