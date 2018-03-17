290d56f9e93028c1ef4276093dbdf70e262bc68e
katwal-dipak <katwal.dipak@hotmail.com>
2018-03-17 11:54:42 +0530

    minor change

--- src/scene/SearchListing/SearchListing.js
+++ src/scene/SearchListing/SearchListing.js
@@ -11,7 +11,9 @@ import {
 import { FeedsCard } from '../../component/FeedsCard';
 
 const dataSource = [
-    { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }
+    { id: 1, url: 'https://f-static.motosport.com/motoblog/2014/How-to-Buy-a-Used-Dirt-Bike-small.jpg' },
+    { id: 2, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOkswQlV67eHn0SceshdrRBD4s5yc8nJ0_c8lmkEFO2X9xtNBNlA' },
+    { id: 3, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk1DTcYPln78dN1XHJyJYc6L2ERWCa4bCNZyLrAqcEq5HIXm3g' }, { id: 4 }, { id: 5 }, { id: 6 }
 ]
 
 class SearchListing extends Component {
@@ -24,7 +26,7 @@ class SearchListing extends Component {
             time='24 days ago'
             name='Dipak Katwal'
             title='iPhone 8 , box Pack'
-            imageURL='https://img2.carmax.com/img/vehicles/15213982/1/320.jpg'
+            imageURL={item.url}
             thumbnailURL='https://img2.carmax.com/img/vehicles/15213982/1/320.jpg'
         />
     }
