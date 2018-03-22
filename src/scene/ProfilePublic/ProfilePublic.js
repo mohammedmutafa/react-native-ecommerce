199e6263bde68c17149a0a8dd1ec852499ccee0b
katwal-dipak <katwal.dipak@hotmail.com>
2018-03-22 13:13:32 +0530

    Minor Changes

--- src/scene/ProfilePublic/ProfilePublic.js
+++ src/scene/ProfilePublic/ProfilePublic.js
@@ -12,9 +12,7 @@ import { Avatar, Icon } from 'react-native-elements';
 
 import styles from './styles';
 import colors from '../../styles/Color';
-
 import { screenHeight, screenWidth } from '../../utilities/ScreenSize';
-
 import Categories from '../../styles/Categories';
 
 const { MainCategory } = Categories;
@@ -49,60 +47,77 @@ class ProfilePublic extends Component {
         );
     }
 
+    renderProfileHeader = () => {
+        return (
+            <View style={triangleDown}>
+                <View style={circleContainer}>
+                    <View style={circle} >
+                        <Avatar
+                            rounded
+                            width={100}
+                            height={100}
+                            containerStyle={avatarStyle}
+                            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg" }}
+                            onPress={() => console.log("Works!")}
+                            activeOpacity={0.7}
+                        />
+                    </View>
+                </View>
+            </View>
+        );
+    }
+
+    renderUserBasicInfo = () => {
+        return (
+            <View style={{ flexDirection: 'column' }}>
+                <Text style={nameTextStyle}>Dipak Katwal</Text>
+                <Text style={addressTextStyle}>Kathmandu, Nepal</Text>
+            </View>
+        );
+    }
+
+    renderSocialButtons = () => {
+        return (
+            <View style={socialButtonsContainer}>
+                <Icon
+                    raised
+                    name='call'
+                    type='material'
+                    color={colors.lightWhite}
+                    containerStyle={iconContainerStyle}
+                    onPress={() => console.log('hello')} />
+                <Icon
+                    raised
+                    name='sc-facebook'
+                    type='evilicon'
+                    color={colors.lightWhite}
+                    containerStyle={iconContainerStyle}
+                    onPress={() => console.log('hello')} />
+                <Icon
+                    raised
+                    name='logo-instagram'
+                    type='ionicon'
+                    color={colors.lightWhite}
+                    containerStyle={iconContainerStyle}
+                    onPress={() => console.log('hello')} />
+                <Icon
+                    raised
+                    name='email'
+                    type='zocial'
+                    color={colors.lightWhite}
+                    containerStyle={iconContainerStyle}
+                    onPress={() => console.log('hello')} />
+            </View>
+        );
+    }
+
     render() {
         return (
             <View style={conatinerStyle}>
-                <View style={triangleDown}>
-                    <View style={circleContainer}>
-                        <View style={circle} >
-                            <Avatar
-                                rounded
-                                width={100}
-                                height={100}
-                                containerStyle={avatarStyle}
-                                source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg" }}
-                                onPress={() => console.log("Works!")}
-                                activeOpacity={0.7}
-                            />
-                        </View>
-                    </View>
-                </View>
-
-                <Text style={nameTextStyle}>Dipak Katwal</Text>
-                <Text style={addressTextStyle}>Kathmandu, Nepal</Text>
-
-                <View style={socialButtonsContainer}>
-                    <Icon
-                        raised
-                        name='call'
-                        type='material'
-                        color={colors.lightWhite}
-                        containerStyle={iconContainerStyle}
-                        onPress={() => console.log('hello')} />
-                    <Icon
-                        raised
-                        name='sc-facebook'
-                        type='evilicon'
-                        color={colors.lightWhite}
-                        containerStyle={iconContainerStyle}
-                        onPress={() => console.log('hello')} />
-                    <Icon
-                        raised
-                        name='logo-instagram'
-                        type='ionicon'
-                        color={colors.lightWhite}
-                        containerStyle={iconContainerStyle}
-                        onPress={() => console.log('hello')} />
-                    <Icon
-                        raised
-                        name='email'
-                        type='zocial'
-                        color={colors.lightWhite}
-                        containerStyle={iconContainerStyle}
-                        onPress={() => console.log('hello')} />
-                </View>
+                {this.renderProfileHeader()}
+                {this.renderUserBasicInfo()}
+                {this.renderSocialButtons()}
                 <View style={dividerStyle} />
-
                 {this.renderPublishedPostList()}
             </View>
         );
