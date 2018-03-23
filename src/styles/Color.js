b3cbc9d5e9e7882b85489594127cc847ece8fd86
katwal-dipak <katwal.dipak@hotmail.com>
2018-03-23 14:02:54 +0530

    semi dark white

--- src/styles/Color.js
+++ src/styles/Color.js
@@ -4,5 +4,6 @@ export default colors = {
     golden: '#DAA520',
     red: '#FF1300',
     lightWhite: '#F7F7F7',
+    semiDarkWhite: '#D7D7D7',
     semiTransparentDarkOverlay: 'rgba(60, 60, 60, 0.5)'
 }
