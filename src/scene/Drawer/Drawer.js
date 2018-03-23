6495da9bd8edeffdbda5dc6306fdedb34d9d9d16
katwal-dipak <katwal.dipak@hotmail.com>
2018-03-23 12:33:01 +0530

    Add Rows

--- src/scene/Drawer/Drawer.js
+++ src/scene/Drawer/Drawer.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity
 } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 import { BackButton } from '../../component/BackButton';
 import styles from './styles';
@@ -17,6 +19,30 @@ class Drawer extends Component {
         super(props);
     }
 
+    renderRow = (title) => {
+        return (
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingHorizontal: 15,
+                marginTop: 20
+            }}>
+                <TouchableOpacity>
+                    <Text style={{ fontSize: 18, color: Color.lightWhite }}>{title}</Text>
+                </TouchableOpacity>
+                <Icon
+                    underlayColor='transparent'
+                    name='chevron-right'
+                    type='evilicon'
+                    color={Color.golden}
+                    size={40}
+                    onPress={() => console.log('Dipak')}
+                />
+            </View>
+        );
+    }
+
     render() {
         return (
             <View style={conatinerStyle}>
@@ -26,6 +52,13 @@ class Drawer extends Component {
                     iconType='ionicon'
                     iconColor={Color.golden}
                     onPress={() => this.props.navigation.navigate('DrawerToggle')} />
+                <View style={{ marginTop: 100 }}>
+                    {this.renderRow('Profile')}
+                    {this.renderRow('Bookmarked Items')}
+                    {this.renderRow('Settings')}
+                    {this.renderRow('About')}
+                    {this.renderRow('Logout')}
+                </View>
             </View>
         );
     }
