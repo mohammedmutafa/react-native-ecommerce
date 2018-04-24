f4fcb58bb4a8b5e44727fbf33551b7def0c184e8
katwal-dipak <katwal.dipak@hotmail.com>
2018-04-24 14:28:43 +0530

    link library

--- android/app/src/main/java/com/ecommerce/MainApplication.java
+++ android/app/src/main/java/com/ecommerce/MainApplication.java
@@ -3,6 +3,7 @@ package com.ecommerce;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.wix.interactable.Interactable;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new LottiePackage(),
             new Interactable(),
             new VectorIconsPackage()
