f2afb89ec6b141b0ded992a0a64c6157290bd9d1
katwal-dipak <katwal.dipak@hotmail.com>
2018-07-10 22:22:59 +0530

    change feeds card UI

--- src/component/FeedsCard/FeedsCard.js
+++ src/component/FeedsCard/FeedsCard.js
@@ -7,7 +7,6 @@ import {
     TouchableOpacity
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { Avatar, Icon } from 'react-native-elements';
 
 import styles from './styles';
 import colors from '../../styles/Color';
@@ -16,13 +15,12 @@ import Fonts from '../../styles/Fonts'
 
 const {
     container,
-    avatarContainer,
-    profileConatiner,
     imageViewContainerStyle,
     imageViewStyle,
-    nameTextStyle,
-    productTitleTextStyle,
-    detailContainerStyle
+    titleTextStyle,
+    dateTextStyle,
+    productDescriptionTextStyle,
+    titleContainerStyle
 } = styles;
 
 export class FeedsCard extends Component {
@@ -35,6 +33,92 @@ export class FeedsCard extends Component {
 
     onPressBookmarkButton = () => {
         this.setState({ bookmarked: !this.state.bookmarked });
+    }
+
+    onCardPress = () => {
+        const {
+            ownerID,
+            price,
+            time,
+            title,
+            thumbnailURL,
+            productDescription,
+            navigation,
+            selectedLocation
+        } = this.props;
+
+        navigation.navigate('GeneralProductDetails',
+            {
+                time: time,
+                ownerID: ownerID,
+                price: price,
+                title: title,
+                productDescription: productDescription,
+                thumbnailURL: thumbnailURL,
+                selectedLocation: selectedLocation
+            })
+
+    }
+
+    renderImageView = () => {
+        const {
+            price,
+            thumbnailURL
+        } = this.props;
+
+        return (
+            <TouchableOpacity
+                style={imageViewContainerStyle}
+                onPress={this.onCardPress}
+            >
+                <Image
+                    source={{ uri: thumbnailURL }}
+                    style={imageViewStyle}
+                />
+                {/*<Icon
+                        underlayColor="transparent"
+                        name={bookmarked ? 'heart' : 'heart-outlined'}
+                        type="entypo"
+                        color={bookmarked ? colors.red : colors.lightDark}
+                        containerStyle={{ position: 'absolute', alignSelf: 'flex-end', padding: 10 }}
+                        onPress={this.onPressBookmarkButton}
+                    />*/}
+                <View style={{ position: 'absolute', alignSelf: 'flex-end', bottom: 0, padding: 10, backgroundColor: 'rgba(60, 60, 60, 0.4)' }}>
+                    <Text
+                        style={{ color: colors.lightWhite, fontSize: 18, fontFamily: Fonts.CharterBT }}
+                    >
+                        {`₹ ${numberWithCommas(price)}`}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    renderTitleContainer = () => {
+        const {
+            title,
+            time
+        } = this.props;
+
+        return (
+            <View style={titleContainerStyle}>
+                <Text
+                    style={titleTextStyle}
+                    ellipsizeMode="tail"
+                    numberOfLines={1}
+                >
+                    {title}
+                </Text>
+                <View style={{ flex: 2 }}>
+                    <Text
+                        style={dateTextStyle}
+                        ellipsizeMode="tail"
+                        numberOfLines={1}
+                    >{time}
+                    </Text>
+                </View>
+            </View>
+        );
     }
 
     render() {
@@ -52,74 +136,16 @@ export class FeedsCard extends Component {
 
         return (
             <View style={container}>
-                <View style={detailContainerStyle}>
-                    <View style={avatarContainer}>
-                        <View style={profileConatiner}>
-                            <Avatar
-                                small
-                                rounded
-                                source={{ uri: thumbnailURL }}
-                                onPress={() => navigation.navigate('ProfilePublic')}
-                                activeOpacity={0.7}
-                            />
-                            <Text
-                                style={nameTextStyle}
-                                ellipsizeMode="tail"
-                                numberOfLines={1}
-                            >
-                                {ownerID}
-                            </Text>
-                        </View>
-                        <Text
-                            style={{ alignSelf: 'flex-end', color: colors.lightDark, fontSize: 10 }}
-                            ellipsizeMode="tail"
-                            numberOfLines={1}
-                        >{time}
-                        </Text>
-                    </View>
-                </View>
-                <TouchableOpacity
-                    style={imageViewContainerStyle}
-                    onPress={() => navigation.navigate('GeneralProductDetails',
-                        {
-                            time: time,
-                            ownerID: ownerID,
-                            price: price,
-                            title: title,
-                            productDescription: productDescription,
-                            thumbnailURL: thumbnailURL,
-                            selectedLocation: selectedLocation
-                        })}
-                >
-                    <Image
-                        source={{ uri: thumbnailURL }}
-                        style={imageViewStyle}
-                    />
-                    {/*<Icon
-                        underlayColor="transparent"
-                        name={bookmarked ? 'heart' : 'heart-outlined'}
-                        type="entypo"
-                        color={bookmarked ? colors.red : colors.lightDark}
-                        containerStyle={{ position: 'absolute', alignSelf: 'flex-end', padding: 10 }}
-                        onPress={this.onPressBookmarkButton}
-                    />*/}
-                    <View style={{ position: 'absolute', alignSelf: 'flex-end', bottom: 0, padding: 10, backgroundColor: 'rgba(60, 60, 60, 0.4)' }}>
-                        <Text
-                            style={{ color: colors.lightWhite, fontSize: 18, fontFamily: Fonts.CharterBT }}
-                        >
-                            {`₹ ${numberWithCommas(price)}`}
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-                <Text
-                    style={productTitleTextStyle}
+                {this.renderTitleContainer()}
+                {this.renderImageView()}
+                {/*<Text
+                    style={productDescriptionTextStyle}
                     ellipsizeMode="tail"
-                    numberOfLines={2}
+                    numberOfLines={1}
                 >
                     {title}
-                </Text>
+                </Text>*/}
             </View>
-
         );
     }
 }
